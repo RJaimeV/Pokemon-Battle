71f59afbb23aa85801fb9ed285aa26a130f03871
RJaimeV <156475750+RJaimeV@users.noreply.github.com>
2025-06-13 17:48:39 -0600

    advance

---
+++ Assets/Scripts/Fighter.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Fighter : MonoBehaviour
+{
+    [SerializeField]
+    private Health _health;
+    [SerializeField]
+    private Animator _characterAnimator;
+    [SerializeField]
+    private Attacks _attacks;
+}
